5358a7c22f4d59e9f14d17604bd1122e68f03fcf
Ahmed Alrehaili <103155773+ahmedove5@users.noreply.github.com>
2022-12-19 10:59:29 +0300

    new virsion

---
+++ js.js
@@ -0,0 +1,9 @@
+// window.location = 'https://alliwan.com.sa'
+var close_button = document.querySelector(".close-button");
+var social_buttons = document.querySelectorAll(".social");
+
+close_button.addEventListener('click',()=>{
+   social_buttons.forEach(function(buttons){
+       buttons.classList.toggle('hide');
+   });
+});
